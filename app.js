b05319ef800cf8a193b3334d4a7d1119610c4783
Jose Flores <jose.flores1634@gmail.com>
2018-01-11 14:17:15 -0800

    added for loop

--- app.js
+++ app.js
@@ -3,63 +3,80 @@
 alert( 'Hi! I\'m going to ask you a few questions.');
 // add touppercase or tolowercase
 
-    var usernNameInput = prompt('What is your name?');
-    console.log('The user entered:' + usernNameInput);
+var usernNameInput = prompt('What is your name?');
+console.log('The user entered:' + usernNameInput.toUpperCase);
 
-    var headExplode = prompt('Do you think your head will explode after this week of instruction?');
-    console.log(usernNameInput + ' answered:' + headExplode);
+var headExplode = prompt('Do you think your head will explode after this week of instruction?');
+console.log(usernNameInput + ' answered:' + headExplode);
 
-     if(headExplode === 'yes' || headExplode === 'Yes' || headExplode === 'YES' || headExplode === 'Y' || headExplode === 'y') {
-        alert('Cool beans!');
-     } else if (headExplode === 'no' || headExplode === 'No' || headExplode === 'NO' || headExplode === 'n' || headExplode === 'N') {
-        alert('Then you must be cooler than cool beans!');
-     } else {
-        alert('Please enter yes or no!');
-     }
+// if else statements
 
-    alert('Hi ' + usernNameInput + ', thank you for visiting my site!');
+if(headExplode === 'yes' || headExplode === 'Yes' || headExplode === 'YES' || headExplode === 'Y' || headExplode === 'y') {
+  alert('Cool beans!');
+} else if (headExplode === 'no' || headExplode === 'No' || headExplode === 'NO' || headExplode === 'n' || headExplode === 'N') {
+  alert('Then you must be cooler than cool beans!');
+} else {
+  alert('Please enter yes or no!');
+}
 
-    var questionTwoAns = prompt('How long have you lived in Washington? Please answer with number and years.');
-    console.log(usernNameInput + ' answer:' + questionTwoAns);
+alert('Hi ' + usernNameInput + ', thank you for visiting my site!');
 
-    alert('Wow! That is a long time ' +  usernNameInput +'!'+ ' Ok, on to the next question.');
+var questionTwoAns = prompt('How long have you lived in Washington? Please answer with number and years.');
+console.log(usernNameInput + ' answer:' + questionTwoAns);
 
-    var questionThreeAns = prompt('How many times have you ridden the Sounder train?');
-    console.log(usernNameInput + ' answer:' + questionThreeAns);
+alert('Wow! That is a long time ' +  usernNameInput +'!'+ ' Ok, on to the next question.');
 
-    alert('Not bad! The Sounder train can be a great way to travel!');
+var questionThreeAns = prompt('How many times have you ridden the Sounder train?');
+console.log(usernNameInput + ' answer:' + questionThreeAns);
 
-    var questionFourAns = prompt('Have you coded before?');
-    console.log(usernNameInput + ' answer:' + questionFourAns);
+alert('Not bad! The Sounder train can be a great way to travel!');
 
-    var questionFiveAns = prompt('Do you prefer coffee or tea?');
-    console.log(usernNameInput + ' answer:' + questionFiveAns);
+var questionFourAns = prompt('Have you coded before?');
+console.log(usernNameInput + ' answer:' + questionFourAns);
 
-    var questionSixAns = prompt('From number 1 through 5, can you guess which is my favorite number?');
-    console.log(usernNameInput + ' answered:' + questionSixAns);
+var questionFiveAns = prompt('Do you prefer coffee or tea?');
+console.log(usernNameInput + ' answer:' + questionFiveAns);
 
-    var favNum = [1, 2, 3, 4, 5];
-    var guessNum = 4;
-    while(guessNum < 4) {
-    alert('Awesome ' + usernNameInput + ', you got it right!');
-    } else if (questionSixAns === '1' || headExplode === '2' || headExplode === '4' || headExplode === '5') {
-      alert('Sorry ' + usernNameInput + ', keep guessing!');
-    }
-    guessNum++;
+// Question 6
 
+var favNum = [5];
+var answerNum = prompt('Guess what my favorite number is! From 1 to 5!');
+var flag;
 
+for (var i = 0; i < favNum.length; i++) {
+  console.log('i is equal to:' + favNum[i]);
 
-
-
-
-
-
-
-
-
-
+  if (answerNum === favNum[0]) {
+    alert('You guessed right!');
+    flag = true;
+    break;
+  }
+}
+  if (!flag) {
+    alert('Wrong! Guess again!');
+    console.log();
+}
 
 
+var movies = ['The Matrix'];
+var answer = prompt('What is one of my favorite movies? Choose from pencil, Die Hard, Total Recall, The Matrix.');
+var flag;
+
+for (var i = 0; i < movies.length; i++) {
+  console.log('each item at each interation:' + movies[i]);
+
+  if (answer === movies[0]) {
+    alert('You guessed right!');
+    flag = true;
+    break;
+  }
+continue;
+}
+
+if (!flag) {
+  alert('Wrong! Guess again!');
+  console.log();
+}
 
 
 
@@ -78,6 +95,25 @@ alert( 'Hi! I\'m going to ask you a few questions.');
 
 
 
-    // alert('Well alright! So far ' + usernNameInput + ', we have learned that you answered "' + headExplode +
-    // '" to my first question. You have lived in Washington for ' + questionTwoAns + ' and you have ridden the Sounder train ' +
-    // questionThreeAns + ' times! You also answered "' + questionFourAns + '" to my coding question and you prefer ' + questionFiveAns + '. I think I learned a lot about you today! Now you will learn a little about me.');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert('Well alright! So far ' + usernNameInput + ', we have learned that you answered "' + headExplode +
+// '" to my first question. You have lived in Washington for ' + questionTwoAns + ' and you have ridden the Sounder train ' +
+// questionThreeAns + ' times! You also answered "' + questionFourAns + '" to my coding question and you prefer ' + questionFiveAns + '. I think I learned a lot about you today! Now you will learn a little about me.');
